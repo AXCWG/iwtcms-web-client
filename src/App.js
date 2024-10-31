ce4644584072fda9dcd6134c2efc98dda94702d8
axcwg <xiey0@qq.com>
2024-10-31 20:26:35 +0800

    yet can't be done because currently mod does not run in 1.21.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  function stupidassiterator(){
+    let pending = ""
+    for(let i=0; i<40; i++){
+      pending+="pending\n"
+    }
+    return pending
+  }
+  const [loginInfo, setLoginInfo] = useState({
+    host: "", 
+    password : ""
+  })
+  function loginToServer(){
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container mt-5'>
+        <form className='p-5 rounded-3, ' style={{ maxWidth: "700px", backgroundColor: "#F8F8F8", borderStyle: "solid", borderColor: "#E8E8E8", borderWidth: "1px", margin: "auto", }}>
+          <input className='form-control' placeholder='Host'></input>
+          <input className='form-control mt-3' placeholder='Password'></input>
+          <button className='btn btn-primary w-100 mt-3'>Connect</button>
+          <button className='btn btn-primary w-100 mt-5'>Send</button>
+          <textarea disabled={true} rows={20}  className='w-100 mt-3 form-control '  value={stupidassiterator()}></textarea>
+        </form>
+      </div>
+    </>
   );
 }
 
